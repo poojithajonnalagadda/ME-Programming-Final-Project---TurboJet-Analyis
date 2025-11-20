f34c71506b32ae85ab25803f5c72df053b25f71c
poojithajonnalagadda <poojitha.jonnalagadda@utexas.edu>
2025-11-19 23:07:55 -0600

    Add files via upload

---
+++ graphs.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+def plot_temperature_stations(results):
+    
+def plot_pressure_stations(results):
+    
+def plot_ts_diagram(results):
+    
+#Need to add this feature:
+# Hold vs clear function to compare plots of different runs on the same plot
+# Easier to compare different analyses against each other
+
