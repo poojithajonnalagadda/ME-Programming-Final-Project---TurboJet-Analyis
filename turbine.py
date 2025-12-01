0dffbf1519668c9b17256533e26221afad153669
poojithajonnalagadda <poojitha.jonnalagadda@utexas.edu>
2025-12-01 01:38:08 -0600

    Update turbine.py

--- turbine.py
+++ turbine.py
@@ -1,9 +1,12 @@
 from node import Node, ThermoState
 class Turbine(Node):
-    def __init__(self, inlet: Node, eta_t: float = 1.0):
+    def __init__(self, inlet: Node, eta_t: float = 1.0, T03: float = None, T02: float = None, f: float = None):
         super().__init__(inlet, eta_t)
-
-    def get_outlet_conditions(self, T03, T02, f):
+        self.T03 = T03
+        self.T02 = T02
+        self.f = f
+        
+    def get_outlet_conditions(self):
         # Unpack some variables for readability
         inlet = self.get_inlet_conditions()
         P04 = inlet.P0
@@ -11,7 +14,7 @@ class Turbine(Node):
         k = self.fluid.gamma
 
         # Turbine Outlet Temperaure (actual/non-isentropic)
-        T05 = T04 - (T03 - T02) / (1 + f)
+        T05 = T04 - (self.T03 - self.T02) / (1 + self.f)
         
         #Isentropic (constant entropy) Temperature Drop
         T05s = T04 - (T04 - T05) / self.eta
